2b3678bfe3bedaf9a1ea53153699775ea681f198
Pratheek <32567342+pratheekmurthy@users.noreply.github.com>
2020-12-06 11:28:07 +0530

    Add files via upload

---
+++ src/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState,useEffect } from 'react'
+import {Link, BrowserRouter,Route,Redirect,Switch,useHistory} from 'react-router-dom'
+import Dashboard from './Dashboard'
+import Login from './Login'
+
+
+const App=(props)=>{
+    const [login,setLogin] = useState({})
+    
+    useEffect(()=>{
+        const  result =JSON.parse(localStorage.getItem("user"))
+        setLogin(result);
+
+    },[])
+
+   return (<div> 
+        <BrowserRouter>
+                <Route path="/login"  exact={true} render={()=>(<Login />)} />
+                <Route path="/dashboard"  exact={true} render={()=>(<Dashboard />)} />
+                {
+                    login ? ( <Redirect to='/dashboard'/>) : (<Redirect to='/login'/>)
+                }     
+        </BrowserRouter>     
+    </div>)
+}
+
+export default App
---
+++ src/src/Login.js
@@ -0,0 +1,61 @@
+import React,{useState,useEffect} from 'react'
+import { useHistory } from "react-router-dom"
+import axios from 'axios'
+
+
+
+const Login =(props)=>{
+    const [email,setEmail] = useState("")
+    const [users,setUsers] = useState([])
+
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then((response)=>{
+                const result = response.data;
+                setUsers(result)
+            })
+            .catch((err)=>{
+                alert ( err.message)
+            })
+
+    },[])
+
+    const handleEmail =(e)=>{
+        const result =e.target.value;
+        setEmail(result);
+        
+    }
+    
+    const handleSubmit =(e)=>{
+
+        const result = users.find((ele)=>{
+            return ele.email == email;
+        })
+
+        if(result){
+            const result1 = users.filter((ele)=>{
+                return ele.email == email;
+            })
+            const valid = result1[0].id
+            const name = result.name
+            const company = result.company.name
+            const catchPhrase = result.company.catchPhrase
+            const user = {"login" : true, "id" : valid, "email" : email, "name": name, "companyname" :company, }
+            localStorage.setItem("user", JSON.stringify(user))
+        }
+
+        setEmail("")
+    }
+
+    return (<div>
+        <h3>Login</h3>
+        <form onSubmit={handleSubmit}>
+        <input type="email" placeholder="enter your email address" onChange={handleEmail}/><br/>
+        <input type="submit" value="LogIN"/>
+        </form>
+        </div>
+    )
+
+}
+
+export default Login
